3865fd12ec8bfe347850d1a4f29573884980c26a
Media Archive <dev@md-archive.com>
2024-10-12 21:00:43 +0200

    Meidai Central - zero progress

---
+++ composeApp/src/iosMain/kotlin/org/md_archive/meidai/central/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.md_archive.meidai.central
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "MeidaiCentral"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
